24e5e8ec6359fc0f710f9ad5e6eb497aa4892217
Brandon Suarez <brandonsuarezduran@gmail.com>
2024-01-07 09:38:32 -0500

    Initial Commit

---
+++ GetureRecognizerSwiftUITest/GetureRecognizerSwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetureRecognizerSwiftUITestApp.swift
+//  GetureRecognizerSwiftUITest
+//
+//  Created by Brandon Suarez on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GetureRecognizerSwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
